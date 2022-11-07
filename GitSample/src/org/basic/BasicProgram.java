c2fe61c45ac8cf4337a753ad9008774cbd25537b
praveenrajdeek <117369739+praveenrajdeek@users.noreply.github.com>
2022-11-07 20:44:01 +0530

    initial commit

---
+++ GitSample/src/org/basic/BasicProgram.java
@@ -0,0 +1,13 @@
+package org.basic;
+
+public class BasicProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("simply added");
+		System.out.println("basic program");
+	}
+	
+	
+	
+
+}
